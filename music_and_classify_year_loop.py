157f2a9e656304b53c25015c5e6ee4394bc44764
Nathaniel A. Frissell <nathaniel.frissell@scranton.edu>
2023-08-03 04:12:02 +0000

    really ready to try running everything

--- music_and_classify_year_loop.py
+++ music_and_classify_year_loop.py
@@ -12,8 +12,7 @@ import multiprocessing
 import mstid
 from mstid import run_helper
 
-#years = list(range(2019,2023))
-years = list(range(2012,2013))
+years = list(range(2010,2022))
 
 radars = []
 radars.append('cvw')
@@ -75,43 +74,44 @@ for year in years:
     #******************************************************************************#
     # No User Input Below This Line ***********************************************#
     #******************************************************************************#
-    # Generate MSTID List and do rti_interp level processing.
-    run_helper.get_events_and_run(dct_list,process_level='rti_interp',new_list=new_list,
-            recompute=recompute,multiproc=multiproc,nprocs=nprocs)
+    if mstid_index:
+        # Generate MSTID List and do rti_interp level processing.
+        run_helper.get_events_and_run(dct_list,process_level='rti_interp',new_list=new_list,
+                recompute=recompute,multiproc=multiproc,nprocs=nprocs)
 
-    # Reload RTI Data into MongoDb. ################################################
-    if reupdate_db:
+        # Reload RTI Data into MongoDb. ################################################
+        if reupdate_db:
+            for dct in dct_list:
+                mstid.updateDb_mstid_list(multiproc=multiproc,nprocs=nprocs,**dct)
+
         for dct in dct_list:
-            mstid.updateDb_mstid_list(multiproc=multiproc,nprocs=nprocs,**dct)
+            # Determine if each event is good or bad based on:
+            #   1. Whether or not data is available.
+            #   2. Results of pyDARNmusic.utils.checkDataQuality()
+            #       (Ensures radar is operational for a minimum amount of time during the data window.
+            #        Default is to require the radar to be turned off no more than 10 minutes in the
+            #        data window.)
+            #   3. The fraction of radar scatter points present in the data window.
+            #       (Default requires minimum 67.5% data coverage.)
+            #   4. The percentage of daylight in the data window.
+            #       (Default requires 100% daylight in the data window.)
+            mstid.classify.classify_none_events(**dct) 
 
-    for dct in dct_list:
-        # Determine if each event is good or bad based on:
-        #   1. Whether or not data is available.
-        #   2. Results of pyDARNmusic.utils.checkDataQuality()
-        #       (Ensures radar is operational for a minimum amount of time during the data window.
-        #        Default is to require the radar to be turned off no more than 10 minutes in the
-        #        data window.)
-        #   3. The fraction of radar scatter points present in the data window.
-        #       (Default requires minimum 67.5% data coverage.)
-        #   4. The percentage of daylight in the data window.
-        #       (Default requires 100% daylight in the data window.)
-        mstid.classify.classify_none_events(**dct) 
+            # Generate a web page and copy select figures into new directory to make it easier
+            # to evaluate data and see if classification algorithm is working.
+            mstid.classify.rcgb(classification_path=classification_path,**dct)
 
-        # Generate a web page and copy select figures into new directory to make it easier
-        # to evaluate data and see if classification algorithm is working.
-        mstid.classify.rcgb(classification_path=classification_path,**dct)
+        # Run FFT Level processing on unclassified events.
+        run_helper.get_events_and_run(dct_list,process_level='fft',category='unclassified',
+                multiproc=multiproc,nprocs=nprocs)
 
-    # Run FFT Level processing on unclassified events.
-    run_helper.get_events_and_run(dct_list,process_level='fft',category='unclassified',
-            multiproc=multiproc,nprocs=nprocs)
+        # Now run the real MSTID classification.
+        mstid.classify.run_mstid_classification(dct_list,classification_path=classification_path,
+                multiproc=multiproc,nprocs=5)
 
-    # Now run the real MSTID classification.
-    mstid.classify.run_mstid_classification(dct_list,classification_path=classification_path,
-            multiproc=multiproc,nprocs=5)
-
-    print('Plotting calendar plot...')
-    calendar_output_dir = os.path.join(base_dir,'calendar')
-    mstid.calendar_plot(dct_list,db_name=db_name,output_dir=calendar_output_dir)
+        print('Plotting calendar plot...')
+        calendar_output_dir = os.path.join(base_dir,'calendar')
+        mstid.calendar_plot(dct_list,db_name=db_name,output_dir=calendar_output_dir)
 
     # Run actual MUSIC Processing ##################################################
     if music_process:

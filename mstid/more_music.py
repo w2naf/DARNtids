d8a4aceafa9ac24ba1f27e02d07a3ab395989d91
Nathaniel A. Frissell <nathaniel.frissell@scranton.edu>
2023-08-19 13:03:23 +0000

    np.int and np.float to int and float

--- mstid/more_music.py
+++ mstid/more_music.py
@@ -322,14 +322,14 @@ def auto_range(radar,sTime,eTime,dataObj,bad_range_km=500,
     dist    = np.nan_to_num(dist / np.nanmax(dist))
 
     nrPts   = 1000
-    distArr = np.array([],dtype=np.int)
+    distArr = np.array([],dtype=int)
     for rg in range(len(bins)):
         gate    = bins[rg]
         nrGate  = int(np.floor(dist[rg]*nrPts))
         if nrGate < 0:
             nrGate = 0
 
-        distArr = np.concatenate([distArr,np.ones(nrGate,dtype=np.int)*gate])
+        distArr = np.concatenate([distArr,np.ones(nrGate,dtype=int)*gate])
 
     hist,bins           = np.histogram(distArr,bins=bins,density=True)
     hist                = sp.signal.medfilt(hist,kernel_size=11)
@@ -596,6 +596,7 @@ def run_music(radar,sTime,eTime,
             gate_limits = auto_range(radar,sTime,eTime,dataObj,bad_range_km=bad_range_km)
             pyDARNmusic.defineLimits(dataObj,gateLimits=gate_limits)
         except:
+            import ipdb; ipdb.set_trace()
             reject_messages.append('auto_range() computation error.')
             good = False
 
@@ -985,9 +986,9 @@ def calculate_terminator(lats,lons,dates):
 
     shape       = (len(dates),lats.shape[0],lats.shape[1])
 
-    term_lats   = np.zeros(shape,dtype=np.float)
-    term_tau    = np.zeros(shape,dtype=np.float)
-    term_dec    = np.zeros(shape,dtype=np.float)
+    term_lats   = np.zeros(shape,dtype=float)
+    term_tau    = np.zeros(shape,dtype=float)
+    term_dec    = np.zeros(shape,dtype=float)
 
     terminator  = np.ones(shape,dtype=np.bool)
 

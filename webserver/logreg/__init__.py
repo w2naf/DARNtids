011a5412b3df10e5880b0bff287a6bc4efe551ce
Nathaniel A. Frissell <nathaniel.frissell@scranton.edu>
2024-02-05 10:02:58 -0500

    remove unwanted things

--- webserver/logreg/__init__.py
+++
@@ -1,2 +0,0 @@
-from .global_vars import *
-from .logreg import *
--- webserver/open_dataObj.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-filename    = 'static/music/bks/20100601.2000-20100601.2200/bks-20100601.2000-20100601.2200.p'
-
-with open(filename,'rb') as fl:
-    dataObj = pickle.load(fl)
-
-
